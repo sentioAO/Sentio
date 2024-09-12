8da20fd4b4792ada4f999e954f24f3b68ca2fc43
Krish Soni <sonikrish994@gmail.com>
2024-09-13 00:21:27 +0530

    commit

--- src/Pages/Onchain.tsx
+++ src/Pages/Onchain.tsx
@@ -1,6 +1,7 @@
-import { useActiveAddress } from "arweave-wallet-kit"
+import { useActiveAddress } from "arweave-wallet-kit";
 import axios from "axios";
 import { useEffect } from "react";
+import Navbar from "../Components/Navbar";
 
 const Onchain = () => {
     const address = useActiveAddress();
@@ -10,15 +11,26 @@ const Onchain = () => {
     }
     useEffect(() => {
         fetchProcesses();
-        // const processes = axios.post("https://sam-server.azurewebsites.net/getProcesses", { address });
-    }
-        ,)
-    // console.log(processes);
+    }, []);
+
     return (
-        <div>
-            HELLO {address}
-        </div>
-    )
+        <>
+            <div className="app-background">
+                <Navbar />
+                <div>
+                    <h1 className="text-white tracking-widest mt-5 mx-2 my-3 py-3" style={{ fontFamily: "'Roboto'", textAlign: 'left' }}>HELLO {address}</h1>
+                </div>
+
+                <div  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+                    <input 
+                        type="text" 
+                        placeholder="Search..." 
+                        style={{ width: '50%', padding: '10px', fontSize: '16px', borderRadius: '5px', border: '1px solid #ccc' }} 
+                    />
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Onchain
+export default Onchain;

c7f6107553cf5a1ab2131f3e3eaf8fb5c96dcc01
Krish Soni <sonikrish994@gmail.com>
2024-10-25 19:42:38 +0530

    Refactor Hero component and add scroll-to-bottom functionality

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -25,8 +25,13 @@ export function HeroHighlightDemo() {
         <Highlight className="text-black dark:text-white">
           function, of a function, of a function.
         </Highlight><br/>
-        
-        <button className="px-4 py-2 bg-[#9966ff] rounded-lg text-sm hover:bg-white hover:text-[#9966ff] transition-colors duration-300">
+        <button
+          className="px-4 py-2 bg-[#9966ff] rounded-lg text-sm hover:bg-white hover:text-[#9966ff] transition-colors duration-300"
+          onClick={() => {
+           
+            window.scrollTo(0, document.body.scrollHeight);
+          }}
+        >
           Get Sentio Audit
         </button>
       </motion.h1>
